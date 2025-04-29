Fix types for policy gradient and DQL

qaisim/qrl/dq_learning.py
@@ -51,19 +51,20 @@ class DeepQLearning(Module):
     @tf.function(reduce_retracing=True)
     def q_learning_update(
         self,
-        states: NDArray[np.float32],
-        actions: NDArray[np.int32],
-        rewards: NDArray[np.float32],
-        next_states: NDArray[np.float32], done: NDArray[np.float32]
+        states_np: NDArray[np.float32],
+        actions_np: NDArray[np.int32],
+        rewards_np: NDArray[np.float32],
+        next_states_np: NDArray[np.float32],
+        done_np: NDArray[np.float32]
     ) -> None:
         if self.model is None:
             raise RuntimeError("Model not defined")
         
-        states = tf.convert_to_tensor(states)
-        actions = tf.convert_to_tensor(actions)
-        rewards = tf.convert_to_tensor(rewards)
-        next_states = tf.convert_to_tensor(next_states)
-        done = tf.convert_to_tensor(done)
+        states = tf.convert_to_tensor(states_np)
+        actions = tf.convert_to_tensor(actions_np)
+        rewards = tf.convert_to_tensor(rewards_np)
+        next_states = tf.convert_to_tensor(next_states_np)
+        done = tf.convert_to_tensor(done_np)
 
         future_rewards = self.model_target([next_states])
         target_q_values = rewards + (self.gamma * 
@@ -162,6 +163,8 @@ class DeepQLearning(Module):
         generate_episode: EpisodeCallable,
         num_episodes: int, batch_size: int = 16
     ) -> None:
+        if not self.model:
+            raise RuntimeError("Model not defined")
         with tqdm(total=num_episodes, colour="cyan") as pbar:
             for episode_count in range(num_episodes):
                 pbar.set_description(f"Episode [{episode_count + 1}/{num_episodes}]")

qaisim/qrl/policy_gradient.py
@@ -42,17 +42,17 @@ class PolicyGradient(Module):
     @tf.function(reduce_retracing=True)
     def reinforcement_update(
         self,
-        states: NDArray[np.float32],
-        actions: NDArray[np.int32],
-        returns: NDArray[np.float32],
+        states_np: NDArray[np.float32],
+        actions_np: NDArray[np.int32],
+        returns_np: NDArray[np.float32],
         batch_size: int
     ) -> None:
         if self.model is None:
             raise RuntimeError("Model not defined")
         
-        states = tf.convert_to_tensor(states)
-        actions = tf.convert_to_tensor(actions)
-        returns = tf.convert_to_tensor(returns)
+        states = tf.convert_to_tensor(states_np)
+        actions = tf.convert_to_tensor(actions_np)
+        returns = tf.convert_to_tensor(returns_np)
 
         with tf.GradientTape() as tape:
             tape.watch(self.model.trainable_variables)
@@ -74,6 +74,8 @@ class PolicyGradient(Module):
         num_episodes: int, batch_size: int = 10,
         threshold_reward: float = 500.0
     ) -> None:
+        if not self.model:
+            raise RuntimeError("Model not defined")
         with tqdm(total=num_episodes // batch_size, colour="cyan") as pbar:
             for batch in range(num_episodes // batch_size):
                 pbar.set_description(f"Batch [{batch + 1}/{num_episodes // batch_size}]")
@@ -104,6 +106,8 @@ class PolicyGradient(Module):
         generate_episodes: EpisodeCallable,
         num_episodes: int, batch_size: int = 10,
     ) -> None:
+        if not self.model:
+            raise RuntimeError("Model not defined")
         with tqdm(total=num_episodes // batch_size, colour="cyan") as pbar:
             for batch in range(num_episodes // batch_size):
                 pbar.set_description(f"Batch [{batch + 1}/{num_episodes // batch_size}]")