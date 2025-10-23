aca6fb4d898757a660e0a11bb9047ee4cf7e424a
Irwindeep <b22ai022@iitj.ac.in>
2025-10-23 16:02:45 +0530

    fix env steps in testing

--- qaisim/tests/qrl_tests/test_env.py
+++ qaisim/tests/qrl_tests/test_env.py
@@ -2,11 +2,11 @@ import unittest, os
 from qaisim.qrl.qrl_env import QRLEnv
 from qaisim.utils import Dataset
 
+
 class TestQRLEnv(unittest.TestCase):
     def test_env_initialisation(self):
         data_file = data_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "../testing_dataset.csv"
+            os.path.dirname(os.path.abspath(__file__)), "../testing_dataset.csv"
         )
 
         dataset = Dataset(data_file)
@@ -17,8 +17,7 @@ class TestQRLEnv(unittest.TestCase):
 
     def test_env_reset(self):
         data_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "../testing_dataset.csv"
+            os.path.dirname(os.path.abspath(__file__)), "../testing_dataset.csv"
         )
 
         dataset = Dataset(data_file)
@@ -27,18 +26,17 @@ class TestQRLEnv(unittest.TestCase):
         state, _ = qrl_env.reset()
 
         assert len(state) == 5
-    
+
     def test_env_step(self):
         data_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "../testing_dataset.csv"
+            os.path.dirname(os.path.abspath(__file__)), "../testing_dataset.csv"
         )
 
         dataset = Dataset(data_file)
         qrl_env = QRLEnv(dataset)
 
         state, _ = qrl_env.reset()
-        new_state, reward, terminated, _, qtask = qrl_env.step(0)
+        new_state, reward, terminated, _, qtask, _ = qrl_env.step(0)
 
         assert len(state) == 5
         assert len(new_state) == 5
@@ -47,12 +45,14 @@ class TestQRLEnv(unittest.TestCase):
 
         assert qtask["scheduled_qtask"].num_rescheduled == 0
         assert qtask["scheduled_qtask"].exec_start_time > -1
-        assert qtask["scheduled_qtask"].exec_end_time > qtask["scheduled_qtask"].exec_start_time
+        assert (
+            qtask["scheduled_qtask"].exec_end_time
+            > qtask["scheduled_qtask"].exec_start_time
+        )
 
     def test_termination(self):
         data_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            "../testing_dataset.csv"
+            os.path.dirname(os.path.abspath(__file__)), "../testing_dataset.csv"
         )
 
         dataset = Dataset(data_file)
@@ -60,7 +60,7 @@ class TestQRLEnv(unittest.TestCase):
 
         state, _ = qrl_env.reset()
         for i in range(30):
-            new_state, reward, terminated, _, _ = qrl_env.step(i%4)
+            new_state, reward, terminated, _, _, _ = qrl_env.step(i % 4)
             if i == 15:
                 assert any(new_state["qnode_queued_tasks"] > 0)
 
